4bfa4cbc985db7c491045a9a4cd840839567f490
bmw-dev0p <bmw.dev0p@gmail.com>
2024-08-25 15:03:32 -0400

    fixed console.table, uploading images for README

--- src/connection.ts
+++ src/connection.ts
@@ -1,11 +1,12 @@
+// import user postresql info from .env file
 import dotenv from 'dotenv';
 dotenv.config();
 
 // Import and require Pool (node-postgres)
-// We'll be creating a Connection Pool. Read up on the benefits here: https://node-postgres.com/features/pooling
 import pg from 'pg';
 const { Pool } = pg;
 
+// create new instance with credentials
 const pool = new Pool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
@@ -14,6 +15,7 @@ const pool = new Pool({
   port: 5432,
 });
 
+//establish connection to the database
 const connectToDb = async () => {
   try {
     await pool.connect();
@@ -23,7 +25,5 @@ const connectToDb = async () => {
     process.exit(1);
   }
 };
-
-// connectToDb(); ?
 
 export { pool, connectToDb };
